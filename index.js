d49ba7c05a8419295870800da5f335365785f1d6
rahulchoudhary321 <131349881+rahulchoudhary321@users.noreply.github.com>
2025-07-06 23:35:50 +0530

    remove

--- index.js
+++
@@ -1,8 +0,0 @@
-window.addEventListener("DOMContentLoaded", function () {
-  document.getElementById("imgid").addEventListener("mouseover", function () {
-    setTimeout(function () {
-      window.location.href = "second.html";
-    }, 500);
-  });
-});
-    
